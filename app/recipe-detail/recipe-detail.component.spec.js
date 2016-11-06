943849be4d6b3cb7e0635b3bce49bee9948dae75
Dimka18 <dimka18belarus@gmail.com>
2016-11-06 15:21:22 +0300

    -rename and fix tests

--- app/recipe-detail/recipe-detail.component.spec.js
+++ app/recipe-detail/recipe-detail.component.spec.js
@@ -6,29 +6,29 @@ describe('recipeDetail', function () {
   beforeEach(module('recipeDetail'));
 
   // Test the controller
-  describe('PhoneDetailController', function() {
+  describe('RecipeDetailController', function () {
     var $httpBackend, ctrl;
-    var xyzPhoneData = {
-      name: 'phone xyz',
+    var xyzRecipeData = {
+      name: 'recipe xyz',
       images: ['image/url1.png', 'image/url2.png']
     };
 
     beforeEach(inject(function($componentController, _$httpBackend_, $routeParams) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData);
+      $httpBackend.expectGET('phones/xyz.json').respond(xyzRecipeData);
 
       $routeParams.phoneId = 'xyz';
 
       ctrl = $componentController('recipeDetail');
     }));
 
-    it('should fetch the phone details', function() {
+    it('should fetch the recipe details', function () {
       jasmine.addCustomEqualityTester(angular.equals);
 
       expect(ctrl.recipe).toEqual({});
 
       $httpBackend.flush();
-      expect(ctrl.recipe).toEqual(xyzPhoneData);
+      expect(ctrl.recipe).toEqual(xyzRecipeData);
     });
 
   });
