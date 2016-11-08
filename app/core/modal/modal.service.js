49ffcf11bbac746cd6cc4ecb90c221ef3a374f6f
Dmitry Andryanov <dmitry.andryanov@mainsoft.by>
2016-11-08 15:42:06 +0300

    -refactor modal

--- app/core/modal/modal.service.js
+++ app/core/modal/modal.service.js
@@ -1,55 +1,54 @@
-angular.module('core.modal').factory('Modal', ['$uibModal',
-    function ($uibModal) {
+angular.module('core.modal').service('Modal', ['$uibModal', function ($uibModal) {
 
-        var self = this;
+    var self = this;
 
-        self.activate = function (title, body, successCallback, errorCallback) {
+    self.activate = function (title, body, successCallback, errorCallback) {
 
-            self.successCallback = successCallback;
-            self.errorCallback = errorCallback;
+        self.successCallback = successCallback;
+        self.errorCallback = errorCallback;
 
-            var modal = $uibModal.open({
-                animation: true,
-                templateUrl: 'scripts/shared/modal.template.html',
-                controller: 'ModalInstanceController',
-                controllerAs: 'modalController',
-                windowClass: 'center-modal',
-                resolve: {
-                    title: function () {
-                        return title
-                    },
-                    body: function () {
-                        return body;
-                    },
-                    isError: function () {
-                        if (errorCallback) {
-                            return true;
-                        } else {
-                            return false;
-                        }
+        var modal = $uibModal.open({
+            animation: true,
+            templateUrl: 'scripts/shared/modal.template.html',
+            controller: 'ModalInstanceController',
+            controllerAs: 'modalController',
+            windowClass: 'center-modal',
+            resolve: {
+                title: function () {
+                    return title
+                },
+                body: function () {
+                    return body;
+                },
+                isError: function () {
+                    if (errorCallback) {
+                        return true;
+                    } else {
+                        return false;
                     }
                 }
-            });
+            }
+        });
 
-            modal.result.then(function (result) {
-                switch (result) {
-                    case 'close':
-                        if (self.successCallback) {
-                            self.successCallback();
-                        }
-                        break;
-
-                    case 'dismiss': {
-                        if (self.errorCallback) {
-                            self.errorCallback();
-                        }
-                        break;
+        modal.result.then(function (result) {
+            switch (result) {
+                case 'close':
+                    if (self.successCallback) {
+                        self.successCallback();
                     }
+                    break;
 
-                    default:
-                        break;
+                case 'dismiss': {
+                    if (self.errorCallback) {
+                        self.errorCallback();
+                    }
+                    break;
                 }
-            });
-        };
-    }
+
+                default:
+                    break;
+            }
+        });
+    };
+}
 ]);
